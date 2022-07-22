f73cb6a13bce23ab4c3a6cffed43a363c61773c0
HV986979 <108796296+HV986979@users.noreply.github.com>
2022-07-21 20:06:48 -0400

    Update setup_apache.sh

--- setup_apache.sh
+++ setup_apache.sh
@@ -4,6 +4,8 @@ set -x
 
 sudo apt update
 sudo apt install -y apache2
+sudo chown nobody:nogroup /share/log
+echo "/share/log 192.168.1.2(rw,sync,no_root_squash,no_subtree_check)" | sudo tee -a /etc/exports
 
 
 
