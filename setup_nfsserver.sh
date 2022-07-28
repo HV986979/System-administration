6b744e0170bb88dbb483ff84fc34f723c2253c91
HV986979 <108796296+HV986979@users.noreply.github.com>
2022-07-27 20:07:39 -0400

    Update setup_nfsserver.sh

--- setup_nfsserver.sh
+++ setup_nfsserver.sh
@@ -6,5 +6,5 @@ sudo apt update
 sudo apt install -y nfs-common
 mkdir /var/webserver_monitor
 sudo chown nobody:nogroup /var/webserver_monitor
-sudo mount 192.168.1.2:/var/webserver_log /var/webserver_monitor
+sudo mount 192.168.1.1:/var/webserver_log /var/webserver_monitor
 cd /var/webserver_log
