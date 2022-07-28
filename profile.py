b32be93bfa970587c67b23651077244571aeccad
HV986979 <108796296+HV986979@users.noreply.github.com>
2022-07-27 20:16:12 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -16,7 +16,8 @@ for i in range(2):
     
   else: 
     node = request.XenVM("observer")
-    time.sleep(180)
+    
+  time.sleep(180)
     
   node.routable_control_ip = "true"
   node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU20-64-STD"
