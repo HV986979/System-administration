a33a13572c8902a1f2cc3d954c005f0f940d87da
HV986979 <108796296+HV986979@users.noreply.github.com>
2022-07-27 20:50:28 -0400

    Update setup_nfsserver.sh

--- setup_nfsserver.sh
+++ setup_nfsserver.sh
@@ -5,6 +5,6 @@ set -x
 sudo apt update
 sudo apt install -y nfs-common
 mkdir /var/webserver_monitor
-sudo chown nobody:nogroup /var/webserver_monitor
 sudo mount 192.168.1.1:/var/webserver_log /var/webserver_monitor
-cd /var/webserver_log
+cd /var/webserver_monitor
+sudo touch unauthorized.log
