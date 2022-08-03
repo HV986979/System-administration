b3223e38187ac899e0c9a3e7304b2dfcb226a1ec
HV986979 <108796296+HV986979@users.noreply.github.com>
2022-08-03 17:16:05 -0400

    Add files via upload

---
+++ monitor.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+#Initally I created one more file called monitorlog.log which is used to find unauthorized access
+
+duplicatelogcount=`wc -l /var/webserver_monitor/monitorlog.log | awk '{print $1}'`
+unauthlogcount=`wc -l /var/webserver_monitor/unauthorized.log | awk '{print $1}'`
+echo $duplicatelogcount
+echo $unauthlogcount
+if [ "$duplicatelogcount" != "$unauthlogcount" ]
+then
+     
+        diff /var/webserver_monitor/unauthorized.log /var/webserver_monitor/monitorlog.log > new_invalid_access
+        sudo sed -i "1d" new_invalid_access 
+        sudo sed -i s/\<//g  new_invalid_access 
+        mail -s "unauthorized.log updates" -A new_invalid_access harshawcu@gmail.com < new_invalid_access 
+        cp /var/webserver_monitor/unauthorized.log /var/webserver_monitor/monitorlog.log
+else
+        echo "No unauthaccess" | mail -s "No unauthorized access." harshawcu@gmail.com
+fi
