4366d892ddd4e69837971768d8d2b5afdf867fcc
HV986979 <108796296+HV986979@users.noreply.github.com>
2022-08-04 17:19:33 -0400

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 sudo apt install geoip-bin > /dev/null
-sudo grep -ie "invalid user" -ie "unauthorized" -ie "Failed password"   /var/log/auth.log | awk '{print $1" " $2 " " $3 " " $11}' > modify.log
+sudo grep -ie "invalid user" -ie "unauthorized" -ie "Failed password"   /var/log/auth.log | awk '{print $1" " $2 " " $3 " " $11}' > modify_unau.log
 while IFS= read -r line; do
         ip=`echo $line | awk '{print $4}'`
         if [[ $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3} ]]; then
            date=`echo $line | awk '{print $1" " $2 " " $3}'`
         echo $ip,$(geoiplookup -s $ip | cut -d ":" -f2),$date >> /var/webserver_log/unauthorized.log
         fi
-done < "modify.log"
+done < "modify_unau.log"
